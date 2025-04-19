1a19cebc8067fe300ebb649e154733be43651fda
busyhe <busyhe@qq.com>
2025-04-19 21:27:17 +0800

    feat: add husky

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from "@workspace/eslint-config/next-js"
+import { nextJsConfig } from "@m-nav/eslint-config/next-js"
 
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig
--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,4 +1,4 @@
-import { config } from "@workspace/eslint-config/react-internal"
+import { config } from "@m-nav/eslint-config/react-internal"
 
 /** @type {import("eslint").Linter.Config} */
 export default config
