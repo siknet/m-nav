897c922df76ac228b545dc2cfc3254cde5a47d31
busyhe <busyhe@qq.com>
2025-07-09 11:46:50 +0800

    chore: update turbo version

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -3,12 +3,6 @@ const nextConfig = {
   transpilePackages: ["@m-nav/ui"],
   staticPageGenerationTimeout: 300,
   images: {
-    domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      's3.us-west-2.amazonaws.com'
-    ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
